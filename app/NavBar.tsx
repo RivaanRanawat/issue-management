db5bebbad11f1b6ca7e6e6872fa8843252b0ddbc
Rivaan Ranawat <namanrivaan@gmail.com>
2023-11-06 00:59:09 -0500

    fixing navbar ui

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { GoIssueReopened } from "react-icons/go";
 import AuthState from "./components/AuthState";
+import { Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const navbarItems = [
@@ -20,24 +21,28 @@ const NavBar = () => {
   const path = usePathname();
 
   return (
-    <nav className="flex space-x-6 border-b px-5 mb-5 h-14 items-center">
-      <Link href="/">
-        <GoIssueReopened />
-      </Link>
-      <ul className="space-x-6">
-        {navbarItems.map((item) => (
-          <Link
-            key={item.title}
-            href={item.href}
-            className={`hover:text-zinc-800 ${
-              item.href == path ? "text-zinc-900" : "text-zinc-500"
-            }`}
-          >
-            {item.title}
+    <nav className="border-b px-5 mb-5 py-3">
+      <Flex justify={"between"}>
+        <Flex justify={"center"} gap={"5"}>
+          <Link href="/" className="py-1">
+            <GoIssueReopened />
           </Link>
-        ))}
-      </ul>
-      <AuthState />
+          <ul className="flex space-x-6">
+            {navbarItems.map((item) => (
+              <Link
+                key={item.title}
+                href={item.href}
+                className={`hover:text-zinc-800 ${
+                  item.href == path ? "text-zinc-900" : "text-zinc-500"
+                }`}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </ul>
+        </Flex>
+        <AuthState />
+      </Flex>
     </nav>
   );
 };
--- app/components/AuthState.tsx
+++ app/components/AuthState.tsx
@@ -20,28 +20,26 @@ const AuthState = () => {
   }
 
   return (
-    <Box>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger>
-          <Avatar
-            src={session!.user!.image!}
-            fallback="?"
-            size="2"
-            radius="full"
-            className="cursor-pointer"
-            referrerPolicy="no-referrer"
-          />
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content>
-          <DropdownMenu.Label>
-            <Text size="2">{session!.user!.email}</Text>
-          </DropdownMenu.Label>
-          <DropdownMenu.Item>
-            <Link href="/api/auth/signout">Log out</Link>
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
-    </Box>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Avatar
+          src={session!.user!.image!}
+          fallback="?"
+          size="2"
+          radius="full"
+          className="cursor-pointer"
+          referrerPolicy="no-referrer"
+        />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Label>
+          <Text size="2">{session!.user!.name}</Text>
+        </DropdownMenu.Label>
+        <DropdownMenu.Item>
+          <Link href="/api/auth/signout">Log out</Link>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 };
 
